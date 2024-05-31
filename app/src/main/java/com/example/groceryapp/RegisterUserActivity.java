2a9383c854a9e1a2b76f9953782004cd17e99cb1
tamtoanlenguyen@gmail.com <you@example.com>
2024-05-31 15:44:33 +0700

    login1

--- app/src/main/java/com/example/groceryapp/RegisterUserActivity.java
+++ app/src/main/java/com/example/groceryapp/RegisterUserActivity.java
@@ -1,6 +1,13 @@
 package com.example.groceryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterUserActivity extends AppCompatActivity {
+    private ImageButton backBtn, gpsBtn;
+    private ImageView profileIv;
+    private EditText nameEt, phoneEt, countryEt, stateEt, addressEt,cityEt, emailEt, paswordEt, cPasswordEt;
+    private Button registerBtn;
+    private TextView registerSellerTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,52 @@ public class RegisterUserActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        backBtn = findViewById(R.id.backBtn);
+        gpsBtn = findViewById(R.id.gpsBtn);
+        profileIv = findViewById(R.id.profileIv);
+        nameEt = findViewById(R.id.nameEt);
+        nameEt = findViewById(R.id.nameEt);
+        phoneEt = findViewById(R.id.phoneEt);
+        countryEt = findViewById(R.id.countryEt);
+        stateEt = findViewById(R.id.stateEt);
+        cityEt = findViewById(R.id.cityEt);
+        addressEt = findViewById(R.id.addressEt);
+        emailEt = findViewById(R.id.emailEt);
+        paswordEt = findViewById(R.id.passwordEt);
+        cPasswordEt = findViewById(R.id.cPasswordEt);
+        registerBtn = findViewById(R.id.regiterBtn);
+        registerSellerTv=findViewById(R.id.registerSellerTv);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        gpsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //detect curent location
+            }
+        });
+        profileIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Pick image
+            }
+        });
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //register user
+            }
+        });
+        registerSellerTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //open register seller activity
+                startActivity(new Intent(RegisterUserActivity.this, RegisterSellerActivity.class));
+            }
         });
     }
 }
